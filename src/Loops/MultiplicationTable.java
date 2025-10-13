8942609e5cddd478d6199e85efaabd4844a8772b
Admin <tu60prbs1@code.edu.az>
2025-10-13 22:23:10 +0400

    Loop tasks completed

---
+++ src/Loops/MultiplicationTable.java
@@ -0,0 +1,14 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = input.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number*i);
+        }
+    }
+}
